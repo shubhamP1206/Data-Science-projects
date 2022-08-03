e19322f0ca4fad40f943e2700c7379012fb91a6b
Shubham Pattanayak <97521152+shubhamP1206@users.noreply.github.com>
2022-08-03 20:54:18 +0530

    Add files via upload

---
+++ Coursera.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **SHUBHAM PATTANAYAK**
+# Data Analyst
+
+# *I am interested in data science because i am very passionate towards this field. *
+
+# # The below code will print ‘Hello World’
+
+# In[1]:
+
+
+print("Hello World")
+
+
+# code -
+# a = 5
+# b = 5
+# c = a+b
+# print(c)
+# 
+# ~~strikethrough
+# >Block quotes<
